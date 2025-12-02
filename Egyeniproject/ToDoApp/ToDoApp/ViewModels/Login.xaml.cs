7bfe296f328942d2fb42ec4b0e56aae82d8addbd
kovacs marcell <c0kovmar@vasvari.org>
2025-12-02 13:10:29 +0100

    minor adjustments

---
+++ Egyeniproject/ToDoApp/ToDoApp/ViewModels/Login.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+using System.Net.Http.Json;
+using System.IO;
+using System.Text;
+
+
+namespace ToDoApp;
+
+public partial class Login 
+{
+ 
+    public Login()
+    {
+        InitializeComponent();
+    
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    private async void OnRegClicked(object sender, EventArgs e)
+    {
+      
+    }
+}
---
+++ Egyeniproject/ToDoApp/ToDoApp/ViewModels/Register.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Net.Http.Json;
+
+namespace ToDoApp;
+
+public partial class Register
+{
+
+    public Register()
+    {
+        InitializeComponent();
+        
+    }
+
+    private async void OnSubmitClicked(object sender, EventArgs e)
+    {
+       
+       
+        
+    }
+    private async void OnLogClicked(object sender, EventArgs e)
+    {
+      
+    }
+}
+
+
+
+

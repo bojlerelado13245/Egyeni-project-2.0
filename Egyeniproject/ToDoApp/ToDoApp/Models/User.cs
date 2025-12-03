5ab2a07de7083d721332d2ebf571eee295fd4cc5
kovacs marcell <c0kovmar@vasvari.org>
2025-12-03 10:45:40 +0100

    itt is van

--- Egyeniproject/ToDoApp/ToDoApp/Models/User.cs
+++ Egyeniproject/ToDoApp/ToDoApp/Models/User.cs
@@ -4,7 +4,6 @@ public class User
 {
     public Guid Id { get; set; }
     public string UserName { get; set; }
-    
     public string Email { get; set; }
     public string Password { get; set; }
 }
